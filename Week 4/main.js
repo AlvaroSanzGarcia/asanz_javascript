faacee2643985e0151de74100de585df2d6605f5
Alvaro Sanz Garcia <fe7575qi@go.minnstate.edu>
2025-09-18 14:47:52 -0500

    Completed Exercise 4

--- Week 4/main.js
+++ Week 4/main.js
@@ -1,85 +1,102 @@
 "use strict";
 
-window.onload = function () {
-    // First card - DOM Traversal
-    let cardContainer1 = document.getElementById("card1");
-    let cardHeader1 = document.createElement("div");
-    cardHeader1.className = "card-header";
-    cardHeader1.textContent = "DOM Traversal"
-    cardHeader1.style.fontWeight = "bold";
-    cardContainer1.appendChild(cardHeader1);
+// DOM traversal: ***function skips empty nodes resulting from line breaks in the html file***
+document.getElementById("log-button").addEventListener("click", function () {
+    let output = document.getElementById("card2");
+    document.getElementById("result-field").textContent = "";
+    traverseDOM(output);
+});
 
-    let cardBody1 = document.createElement("div");
-    cardBody1.className = "card-body";
-    cardContainer1.appendChild(cardBody1);
-
-    let inputGroup1 = document.createElement("div");
-    inputGroup1.className = "input-group mb-3";
-    
-    let inputField = document.createElement("input");
-    inputField.type = "text";
-    inputField.id = "textField";
-    inputField.className = "form-control";
-    inputField.placeholder = "Enter text";
-    inputField.addEventListener("input", function() {
-        let output = document.getElementsByClassName("result-group");
-        for (let i = 0; i < output.length; i++) {
-            output[i].textContent = document.getElementById("textField").value;
+function traverseDOM(element) {
+    if (element.nodeType === Node.TEXT_NODE) {
+        const trimmed = element.textContent.trim();
+        if (trimmed !== "") {
+            console.log(element.textContent);
+            document.getElementById("result-field").innerHTML += trimmed + "<br>";
         }
-    });
-    inputGroup1.appendChild(inputField);
-
-    cardBody1.appendChild(inputGroup1);   
-     
-
-    for (let i = 0; i < 3; i++) {
-        let resultGroup = document.createElement("div");
-        resultGroup.className = "input-group mb-3 result-group";
-
-        let resultField = document.createElement("div");
-        resultField.textContent = "text to be replaced";
-
-        resultGroup.appendChild(resultField);
-        cardBody1.appendChild(resultGroup);
-    }  
-
-
-
-
-    // Second card - DOM Modification
-    let cardContainer2 = document.getElementById("card2");
-
-    let cardHeader2 = document.createElement("div");
-    cardHeader2.className = "card-header";
-    cardHeader2.textContent = "DOM Modification";
-    cardHeader2.style.fontWeight = "bold";
-    cardContainer2.appendChild(cardHeader2);
-
-    let cardBody2 = document.createElement("div");
-    cardBody2.className = "card-body";
-    cardContainer2.appendChild(cardBody2);
-
-
-    let addItemButton = document.createElement("button");
-    addItemButton.type = "button";
-    addItemButton.className = "btn btn-primary";
-    addItemButton.textContent = "Add Item";
-    addItemButton.addEventListener("click", function () {
-        let item = document.createElement("li");
-        item.className = "list-group-item";
-        item.innerHTML = "Item added";
-        document.getElementById("list-of-items").appendChild(item);
-    });
-    cardBody2.appendChild(addItemButton);
-
-    let listOfItems = document.createElement("ul");
-    listOfItems.className = "list-group list-group-flush";
-    listOfItems.id = "list-of-items";
-    for (let i = 0; i < 4; i++) {
-        let item = document.createElement("li");
-        item.className = "list-group-item";
-        item.innerHTML = "Item " + i;
-        listOfItems.appendChild(item);
+    } else {
+        for (const child of element.childNodes) {
+            traverseDOM(child);
+        }
     }
-    cardBody2.appendChild(listOfItems);
+}
+
+
+// DOM modification
+document.getElementById("addItemButton").addEventListener("click", function () {
+    let item = document.createElement("li");
+    item.className = "list-group-item";
+    item.textContent = "Item added";
+    document.getElementById("list-of-items").appendChild(item);
+});
+
+
+// Timed updates
+let timeID1 = setInterval(getCurrentDateTime, 1000);
+
+function getCurrentDateTime() {
+    const dateTimeNow = new Date();
+    document.getElementById("time-output").innerHTML = dateTimeNow;
+}
+
+
+// User input
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const response = window.prompt("Please confirm your password", "Password");
+    if (response) {
+        window.alert("Your password has been saved");
+    } else {
+        window.alert("Your password could not be saved");
+    }
+});
+
+
+// Window properties
+let timeID2 = setInterval(getScreenSize, 1000);
+
+function getScreenSize() {
+    const screenWidth = screen.width;
+    let screenType;
+
+    if (screenWidth < 480) {
+        screenType = "mobile device";
+    } else if (screenWidth < 1024) {
+        screenType = "tablet";
+    } else {
+        screenType = "desktop";
+    }
+
+    document.getElementById("window-properties").innerHTML =
+        `Screen width: <span style="color: #007bff;">${screenWidth} px</span><br>
+         Screen type: <span style="color: #007bff;">${screenType}</span>`;
+}
+
+
+// Navigation history
+document.getElementById("go-back-button").addEventListener("click", function () {
+    history.back()
+});
+
+
+// Bonus: Boostrap modals
+const toastTrigger1 = document.getElementById('close-button');
+const toastLiveExample1 = document.getElementById('liveToast1');
+
+if (toastTrigger1) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample1)
+  toastTrigger1.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+
+const toastTrigger2 = document.getElementById('save-button');
+const toastLiveExample2 = document.getElementById('liveToast2');
+
+if (toastTrigger2) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample2)
+  toastTrigger2.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
 }
